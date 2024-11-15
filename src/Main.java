318fa9fb102803cb529fc8432609a874d4b2ba5f
Mikael Edvardsson <Mikael.Edvardsson@elev.ga.ntig.se>
2024-11-15 14:51:47 +0100

    logik

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,23 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-    }
+
 
 
     Hero hero = new Hero();
     Enemy enemy = new Enemy();
 
+    //namn av hero
     hero.name = "Hero!";
-}
+
+    //namn av enemy
+    enemy.name = "enemy!";
+
+
+        System.out.println(hero.name + " meets" + enemy.name + " who has " + enemy.hp + "hp");
+
+
+
+    }
+
 }
